4c55178159e7961b0a97daa87ac3ea71f5a3a77a
Zhen Yang <webyangfullstack@gmail.com>
2022-10-20 18:20:13 +1100

    build home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "feec8b68f6ee71d1"; }
+/******/ 		__webpack_require__.h = function() { return "c1cc52ce7f526baf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
